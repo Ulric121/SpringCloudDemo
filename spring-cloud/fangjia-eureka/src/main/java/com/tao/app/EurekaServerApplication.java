4e8fc6108f7b499d2bb88e8ca875c1a419b9c3e4
yangtao <1376052489@qq.com>
2018-10-22 21:34:51 +0800

    first commit

--- spring-cloud/fangjia-eureka/src/main/java/com/tao/app/EurekaServerApplication.java
+++ spring-cloud/fangjia-eureka/src/main/java/com/tao/app/EurekaServerApplication.java
@@ -1,4 +1,4 @@
-package com.tao.app.app;
+package com.tao.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- spring-cloud/fangjia-fsh-substitution-service/src/main/java/com/tao/controller/SubstitutionController.java
+++ spring-cloud/fangjia-fsh-substitution-service/src/main/java/com/tao/controller/SubstitutionController.java
@@ -1,9 +1,8 @@
 package com.tao.controller;
 
+import com.tao.entity.HouseInfo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 /**
@@ -16,8 +15,30 @@ public class SubstitutionController {
     @Autowired
     private RestTemplate restTemplate;
 
-    @RequestMapping(value = "callHello", method = RequestMethod.GET)
+    @RequestMapping(value = "/callHello", method = RequestMethod.GET)
     public String callHello() {
-        return restTemplate.getForObject("http://fsh-house/house/hello", String.class);
+        String result = restTemplate.getForObject("http://fsh-house/house/hello", String.class);
+        System.out.println("调用结果：" + result);
+        return result;
+    }
+
+    @RequestMapping(value = "/data", method = RequestMethod.GET)
+    public HouseInfo getData(@RequestParam("name") String name) {
+        return restTemplate.getForObject("http://fsh-house/house/data?name=" + name, HouseInfo.class);
+    }
+
+    @RequestMapping(value = "/data/{name}", method = RequestMethod.GET)
+    public String getData2(@PathVariable("name") String name) {
+        return restTemplate.getForObject("http://fsh-house/house/data/{name}", String.class, name);
+    }
+
+    @RequestMapping(value = "/save", method = RequestMethod.GET)
+    public Long add() {
+        HouseInfo houseInfo = new HouseInfo();
+        houseInfo.setCity("上海");
+        houseInfo.setArea("虹口");
+        houseInfo.setAddress("东体小区");
+        Long id = restTemplate.postForObject("http://fsh-house/house/save", houseInfo, Long.class);
+        return id;
     }
 }
